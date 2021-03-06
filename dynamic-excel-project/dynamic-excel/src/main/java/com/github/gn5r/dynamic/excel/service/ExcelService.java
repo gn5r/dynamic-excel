dc6cb6cdd5d0fde6b9eb8cba8fafc0c4708645fc
gn5r <shangyuan.tuolang@gmail.com>
2021-03-07 07:28:09 +0900

    セルの値取得を指定した型で取得するしょりを追加

--- dynamic-excel-project/dynamic-excel/src/main/java/com/github/gn5r/dynamic/excel/service/ExcelService.java
+++ dynamic-excel-project/dynamic-excel/src/main/java/com/github/gn5r/dynamic/excel/service/ExcelService.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -56,13 +57,13 @@ public class ExcelService {
             Object month = this.getCell(sheet, 経費Enum.MONTH.getCellName());
             map.put("何月度経費", month);
             // 作成日
-            Object createDate = this.getCell(sheet, 経費Enum.CREATE_DATE.getCellName());
+            String createDate = this.getCell(sheet, 経費Enum.CREATE_DATE.getCellName(), String.class);
             map.put("作成日", createDate);
             // 従業員名
             Object empName = this.getCell(sheet, 経費Enum.EMPLOYEE_NAME.getCellName());
             map.put("従業員名", empName);
             // 発行日
-            Object issueDate = this.getCell(sheet, 経費Enum.ISSUE_DATE.getCellName());
+            Date issueDate = this.getCell(sheet, 経費Enum.ISSUE_DATE.getCellName(), Date.class);
             map.put("発行日", issueDate);
             // 内容
             Object content = this.getCell(sheet, 経費Enum.CONTENT.getCellName());
@@ -71,10 +72,10 @@ public class ExcelService {
             Object roundTripDiv = this.getCell(sheet, 経費Enum.ROUND_TRIP_DIV.getCellName());
             map.put("往復区分", roundTripDiv);
             // 金額
-            Object cost = this.getCell(sheet, 経費Enum.COST.getCellName());
+            String cost = this.getCell(sheet, 経費Enum.COST.getCellName(), String.class);
             map.put("金額", cost);
             // 合計
-            Object total = this.getCell(sheet, 経費Enum.TOTAL.getCellName());
+            Integer total = this.getCell(sheet, 経費Enum.TOTAL.getCellName(), Integer.class);
             map.put("合計", total);
             // 結合セル
             Object joinCell = this.getCell(sheet, 経費Enum.JOIN_CELL.getCellName());
@@ -117,6 +118,171 @@ public class ExcelService {
             }
         }
         return null;
+    }
+
+    public Object getCell(Sheet sheet, String position, CELL_DESTINATION_TYPE destinationType) {
+        CellReference ref = new CellReference(position);
+        Row row = sheet.getRow(ref.getRow());
+        if (row != null) {
+            Cell cell = row.getCell(ref.getCol());
+            if (cell != null) {
+                CellType type = cell.getCellType();
+                // セルが日付型
+                if (type == CellType.STRING) {
+                    return this.parseDestinationType(cell.getStringCellValue(), destinationType);
+                } else if (type == CellType.NUMERIC) {
+                    if (DateUtil.isCellDateFormatted(cell)) {
+                        Date date = cell.getDateCellValue();
+                        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+                        return this.parseDestinationType(format.format(date), destinationType);
+                    } else {
+                        return this.parseDestinationType(cell.getNumericCellValue(), destinationType);
+                    }
+                } else if (type == CellType.FORMULA) {
+                    FormulaEvaluator evaluator = this.helper.createFormulaEvaluator();
+                    CellValue value = evaluator.evaluate(cell);
+                    if (value.getCellType() == CellType.STRING) {
+                        return this.parseDestinationType(value.getStringValue(), destinationType);
+                    } else if (value.getCellType() == CellType.NUMERIC) {
+                        return this.parseDestinationType(value.getNumberValue(), destinationType);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public <D> D getCell(Sheet sheet, String position, Class<D> destinationType) {
+        CellReference ref = new CellReference(position);
+        Row row = sheet.getRow(ref.getRow());
+        if (row != null) {
+            Cell cell = row.getCell(ref.getCol());
+            CellType type = cell.getCellType();
+            if (type == CellType.STRING) {
+                // 文字列
+                return this.parseDestinationType(cell.getStringCellValue(), destinationType);
+            } else if (type == CellType.NUMERIC) {
+                // 数値
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    // 日付
+                    return this.parseDestinationType(cell.getDateCellValue(), destinationType);
+                } else {
+                    return this.parseDestinationType(cell.getNumericCellValue(), destinationType);
+                }
+            } else if (type == CellType.FORMULA) {
+                // 関数
+                FormulaEvaluator evaluator = this.helper.createFormulaEvaluator();
+                CellValue value = evaluator.evaluate(cell);
+                if (value.getCellType() == CellType.STRING) {
+                    // 関数の結果が文字列
+                    return this.parseDestinationType(value.getStringValue(), destinationType);
+                } else if (value.getCellType() == CellType.NUMERIC) {
+                    // 関数の結果が数値
+                    return this.parseDestinationType(value.getNumberValue(), destinationType);
+                }
+            }
+        }
+        return null;
+    }
+
+    private <D> D parseDestinationType(Object value, Class<D> destinationType) {
+        if (destinationType != null) {
+            log.info("value:" + value + " ターゲットクラス:" + destinationType);
+
+            if (destinationType.equals(Integer.class)) {
+                if (value instanceof Double) {
+                    final Double d = (Double) value;
+                    return destinationType.cast(d.intValue());
+                } else {
+                    return destinationType.cast(value);
+                }
+            } else if (destinationType.equals(Date.class)) {
+                // 変換対象がDateの場合、valueの型によって処理を変える必要がある
+                if (value instanceof Date) {
+                    // Dateであればそのままキャスト
+                    return destinationType.cast(value);
+                } else if (value instanceof String) {
+                    // Stringであれば一度Dateに変換する
+                    SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+                    try {
+                        Date date = format.parse((String) value);
+                        return destinationType.cast(date);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if (destinationType.equals(String.class)) {
+                // 変換対象がDateの場合、valueの型によって処理を変える必要がある
+                if (value instanceof Date) {
+                    // Stringであれば一度Dateに変換する
+                    SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+                    String date = format.format(value);
+                    return destinationType.cast(date);
+                } else {
+                    return destinationType.cast(value.toString());
+                }
+            } else {
+                return destinationType.cast(value);
+            }
+        }
+
+        return null;
+    }
+
+    private Object parseDestinationType(Object value, CELL_DESTINATION_TYPE destinationType) {
+        if (destinationType != null) {
+            Class<?> clazz = destinationType.getType();
+            log.info("ターゲットクラス:" + clazz);
+
+            if (clazz.equals(String.class)) {
+                return value.toString();
+            } else if (clazz.equals(Integer.class)) {
+                if (value instanceof Double) {
+                    final Double d = (Double) value;
+                    return d.intValue();
+                } else {
+                    return (Integer) value;
+                }
+            } else if (clazz.equals(Date.class)) {
+                return (Date) value;
+            } else if (clazz.equals(Double.class)) {
+                return (Double) value;
+            }
+        }
+        return value;
+    }
+
+    public enum CELL_DESTINATION_TYPE {
+
+        /**
+         * String
+         */
+        STRING(String.class),
+
+        /**
+         * Date
+         */
+        DATE(Date.class),
+
+        /**
+         * Integer
+         */
+        INTEGER(Integer.class),
+
+        /**
+         * Double
+         */
+        DOUBLE(Double.class);
+
+        private Class<?> destinationType;
+
+        private CELL_DESTINATION_TYPE(Class<?> destinationType) {
+            this.destinationType = destinationType;
+        }
+
+        public Class<?> getType() {
+            return this.destinationType;
+        }
     }
 
     /**
