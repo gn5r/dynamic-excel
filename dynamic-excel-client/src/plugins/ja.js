439dd520aad9f60a2362d75d68830c98716d7e0e
gn5r <shangyuan.tuolang@gmail.com>
2021-04-05 00:18:50 +0900

    vuetify日本語化プラグイン作成

---
+++ dynamic-excel-client/src/plugins/ja.js
@@ -0,0 +1,74 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true,
+});
+exports.default = {
+  badge: "バッジ",
+  close: "閉じる",
+  dataIterator: {
+    noResultsText: "検索結果が見つかりません",
+    loadingText: "データを取得中です",
+  },
+  dataTable: {
+    itemsPerPageText: "表示件数:",
+    ariaLabel: {
+      sortDescending: "降順",
+      sortAscending: "昇順",
+      sortNone: "ソートされていません",
+      activateNone: "ソートを削除するには有効にしてください",
+      activateDescending: "降順の並び替えのためには有効にしてください",
+      activateAscending: "昇順のソートのためには有効にしてください",
+    },
+    sortBy: "ソート",
+  },
+  dataFooter: {
+    itemsPerPageText: "表示件数:",
+    itemsPerPageAll: "すべて",
+    nextPage: "次",
+    prevPage: "前",
+    firstPage: "最初のページ",
+    lastPage: "最後のページ",
+    pageText: "{0}～{1}件目 / {2}件中",
+  },
+  datePicker: {
+    itemsSelected: "{0}日付選択",
+    nextMonthAriaLabel: "来月",
+    nextYearAriaLabel: "来年",
+    prevMonthAriaLabel: "前月",
+    prevYearAriaLabel: "前年",
+  },
+  noDataText: "データがありません",
+  carousel: {
+    prev: "前の画像",
+    next: "次の画像",
+    ariaLabel: {
+      delimiter: "{0}件目 / {1}件中",
+    },
+  },
+  calendar: {
+    moreEvents: "さらに{0}",
+  },
+  fileInput: {
+    counter: "{0} ファイル",
+    counterSize: "{0} ファイル (合計 {1})",
+  },
+  timePicker: {
+    am: "AM",
+    pm: "PM",
+  },
+  pagination: {
+    ariaLabel: {
+      wrapper: "ページネーションナビゲーション",
+      next: "次のページ",
+      previous: "前のページ",
+      page: "{0}ページ目に移動",
+      currentPage: "現在のページ、ページ {0}",
+    },
+  },
+  rating: {
+    ariaLabel: {
+      icon: "Rating {0} of {1}",
+    },
+  },
+};
--- dynamic-excel-client/src/plugins/vuetify.js
+++ dynamic-excel-client/src/plugins/vuetify.js
@@ -1,7 +1,12 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import ja from "./ja";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  lang: {
+    locales: { ja },
+    current: "ja",
+  },
 });
