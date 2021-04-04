ca0d4476e05d7c6de02af58d554f84c9209475a4
gn5r <shangyuan.tuolang@gmail.com>
2021-04-05 00:14:16 +0900

    Excelテンプレートテーブルとファイル種別マスタを追加

---
+++ DDL/EXCEL_TEMPLATE_TBL.sql
@@ -0,0 +1,10 @@
+﻿create table `EXCEL_TEMPLATE_TBL` (
+  id int not null primary key auto_increment comment '登録順に自動で割り振られるID',
+  `file_type_id` int not null comment 'ファイル種別ID',
+  `ファイル名` varchar(256) not null comment 'ファイル名',
+  `パス` varchar(1024) not null comment 'ファイルディレクトリ',
+  `登録日` datetime comment '登録日',
+  `更新日` datetime comment '更新日',
+  `論理削除フラグ` tinyint(1) comment '論理削除フラグ',
+  CONSTRAINT `ファイル種別紐づけ` FOREIGN KEY (`file_type_id`) REFERENCES `FILE_TYPE_MST` (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Excelテンプレートテーブル'
---
+++ DDL/FILE_TYPE_MST.sql
@@ -0,0 +1,7 @@
+﻿create table `FILE_TYPE_MST` (
+  `id` int not null primary key auto_increment comment '登録順に自動で割り振られるID',
+  `種別名` varchar(128) not null comment 'ファイル種別名',
+  `登録日` datetime comment '登録日',
+  `更新日` datetime comment '更新日',
+  `論理削除フラグ` tinyint(1) comment '論理削除フラグ'
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='ファイル種別マスタ';
